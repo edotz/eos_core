8a7343354696eaa2611516df3a3a7bf29d08b6f5
Hendrik Ficken <hf@medienintegration.biz>
2017-09-20 17:44:50 +0200

    [TASK] Div...

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,12 @@ if (! defined ( 'TYPO3_MODE' )) {
     die ( 'Access denied.' );
 }
 
+/**
+ * Plugin to show preview sections 
+ * of all fluid_content elements
+ * found in this extension
+ * ,hasi
+ */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin (
     'Edotz.' . $_EXTKEY,
     'pi1',
@@ -10,4 +16,14 @@ if (! defined ( 'TYPO3_MODE' )) {
             'Content' => 'index'
     ),
     array ()
+);
+
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    'mod {
+        wizards.newContentElement.wizardItems.groupeos {
+            header = LLL:EXT:eos_core/Resources/Private/Language/locallang_db.xlf:newContentElements.tab.eos_content
+            show = *
+        }
+    }'
 );
